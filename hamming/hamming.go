934c02f109b954daddbc48858afa1e29428117b5
Gayan <gayanhewa@gmail.com>
2018-12-15 23:18:39 +0100

    Refactoring

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -2,22 +2,15 @@ package hamming
 
 import (
 	"errors"
-	"strings"
 )
 
 // Distance should return the hamming distance between two patterns, or -1 if not applicable.
 func Distance(a, b string) (int, error) {
-	a = strings.TrimSpace(a)
-	b = strings.TrimSpace(b)
-
-	if a == "" || b == "" || a == b {
-		return 0, nil
-	}
 	if len(a) != len(b) {
 		return 0, errors.New("unable to calculate hamming distance")
 	}
 
-	distance := 0
+	var distance int
 	for i := range a {
 		if a[i] != b[i] {
 			distance++
