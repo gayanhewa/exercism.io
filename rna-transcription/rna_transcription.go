a46ed7293317188846666931832c8ae8a21858f7
Gayan <gayanhewa@gmail.com>
2018-12-22 23:56:10 +0100

    Refactor it to use a slice of runes and use the string([]rune) to build the string

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -3,16 +3,16 @@ package strand
 // ToRNA will convert DNA to RNA.
 func ToRNA(dna string) string {
 
-	var mapping = map[rune]string{
-		'G': "C",
-		'C': "G",
-		'T': "A",
-		'A': "U",
+	var mapping = map[rune]rune{
+		'G': 'C',
+		'C': 'G',
+		'T': 'A',
+		'A': 'U',
 	}
-	var output string
-	for _, char := range dna {
-		output += mapping[char]
+	output := make([]rune, len(dna))
+	for i, char := range dna {
+		output[i] = mapping[char]
 	}
 
-	return output
+	return string(output)
 }
