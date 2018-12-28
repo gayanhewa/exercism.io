472e0eb334abc5b69ed2666973fc7d3a460fd056
Gayan <gayanhewa@gmail.com>
2018-12-28 22:21:45 +0100

    Drop the mapping

--- etl/etl.go
+++ etl/etl.go
@@ -6,38 +6,38 @@ import (
 
 // Transform will convert the legacy data to the new format.
 func Transform(inputs map[int][]string) map[string]int {
-	mapping := map[string]int{
-		"A": 1,
-		"E": 1,
-		"I": 1,
-		"O": 1,
-		"U": 1,
-		"L": 1,
-		"N": 1,
-		"R": 1,
-		"S": 1,
-		"T": 1,
-		"D": 2,
-		"G": 2,
-		"B": 3,
-		"C": 3,
-		"M": 3,
-		"P": 3,
-		"F": 4,
-		"H": 4,
-		"V": 4,
-		"W": 4,
-		"Y": 4,
-		"K": 5,
-		"J": 8,
-		"X": 8,
-		"Q": 10,
-		"Z": 10,
-	}
+	// mapping := map[string]int{
+	// 	"A": 1,
+	// 	"E": 1,
+	// 	"I": 1,
+	// 	"O": 1,
+	// 	"U": 1,
+	// 	"L": 1,
+	// 	"N": 1,
+	// 	"R": 1,
+	// 	"S": 1,
+	// 	"T": 1,
+	// 	"D": 2,
+	// 	"G": 2,
+	// 	"B": 3,
+	// 	"C": 3,
+	// 	"M": 3,
+	// 	"P": 3,
+	// 	"F": 4,
+	// 	"H": 4,
+	// 	"V": 4,
+	// 	"W": 4,
+	// 	"Y": 4,
+	// 	"K": 5,
+	// 	"J": 8,
+	// 	"X": 8,
+	// 	"Q": 10,
+	// 	"Z": 10,
+	// }
 	output := make(map[string]int)
-	for _, letters := range inputs {
+	for score, letters := range inputs {
 		for _, letter := range letters {
-			output[strings.ToLower(letter)] = mapping[strings.ToUpper(letter)]
+			output[strings.ToLower(letter)] = score
 		}
 	}
 	return output
