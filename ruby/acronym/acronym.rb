9bd3417bd3a1ec8402b349656da4d57d858bff93
Gayan <gayanhewa@gmail.com>
2019-09-13 14:46:08 +0200

    Solution was wrong, I need to be able to create arbitary ABBR

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,15 +1,5 @@
 class Acronym
-  ABR = {
-    'PNG' => 'Portable Network Graphics',
-    'ROR' => 'Ruby on Rails',
-    'FIFO' => 'First In, First Out',
-    'GIMP' => 'GNU Image Manipulation Program',
-    'CMOS' => 'Complementary metal-oxide semiconductor',
-    'ROTFLSHTMDCOALM' => 'Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me',
-    'SIMUFTA' => 'Something - I made up from thin air'
-  }
-
   def self.abbreviate(str)
-    self::ABR.key(str)
+    str.scan(/\b\w/).join.upcase
   end
 end
