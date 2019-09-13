2d175d4474e9abfef547af56b94d8d8fcdee9515
Gayan <gayanhewa@gmail.com>
2019-09-13 14:05:57 +0200

    Ruby has a translate function that will find and replace strings fuuurk.

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,19 +1,6 @@
 class Complement
-  DNA_RNA_MAP = {
-    'G' => 'C',
-    'C' => 'G',
-    'T' => 'A',
-    'A' => 'U'
-  }
-
   def self.of_dna(nucleodite)
     return '' if nucleodite.empty?
-    return self::DNA_RNA_MAP[nucleodite] if nucleodite.length == 1
-
-    output = ''
-    nucleodite.each_char do |c|
-      output = output + self::DNA_RNA_MAP[c]
-    end
-    output
+    nucleodite.tr('GCTA', 'CGAU')
   end
 end
